72e06981dd291ba85100797608b7a5f733958c7a
IgnacioOchoa <onacho87@gmail.com>
2023-01-19 22:41:26 +0100

    Agregadas tiles al campo de pruebas
    Se agregó un set de tiles para construir el escenario. También se
    configuró el enemigo para poderlo mover con el teclado de manera
    provisional hasta ajustar el movimiento y su interacción con los
    objetos.

--- Enemigo.gd
+++ Enemigo.gd
@@ -1,4 +1,27 @@
-extends Area2D
+extends KinematicBody2D
 
-func _process(delta):
-	position.y += -20*delta
+export var speed = 100
+
+func _physics_process(delta):
+	var direction = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+		direction.y = 0
+		rotation = PI/2
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+		direction.y = 0
+		rotation = 3*PI/2
+	if Input.is_action_pressed("move_down"):
+		direction.y += 1
+		direction.x = 0
+		rotation = PI
+	if Input.is_action_pressed("move_up"):
+		direction.y -= 1
+		direction.x = 0
+		rotation = 0
+		
+	if direction.length() > 1:
+		direction = direction.normalized()
+		
+	position += direction * speed * delta
